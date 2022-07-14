61bf32fe943cccd0d26bbc373c066f7c137e092e
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-07-14 13:40:30 +0530

    updates

--- vendor_management_webapp/lib/homepage.dart
+++ vendor_management_webapp/lib/homepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -26,15 +26,142 @@ class _HomePageState extends State<HomePage> {
                 child: Container(
                   color: Colors.red,
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    padding: const EdgeInsets.symmetric(horizontal: 45),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [Text("LOGO")],
+                      children: [
+                        Text("LOGO",
+                            style: TextStyle(
+                                fontFamily: "WorkSans",
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold)),
+                        /*SizedBox(
+                          width: 150,
+                        )*/
+                        Spacer(),
+                        Container(
+                          child: Row(
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {}, child: Text("Search"))
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Account",
+                              style: TextStyle(
+                                  fontFamily: "WorkSans",
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            )),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Cart",
+                              style: TextStyle(
+                                  fontFamily: "WorkSans",
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))
+                      ],
                     ),
                   ),
                 )),
-            Row()
+            SizedBox(
+              height: 45,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                color: Colors.yellow,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Fruits",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Vegetables",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Dairy Products",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Cereals",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Spices",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Oils",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Flowers",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Newspapers",
+                            style: TextStyle(
+                              fontSize: 17,
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Container(color: Colors.greenAccent),
+            )
           ],
         ),
       )),
