a812db3b6d6bc512f520adfeeb64649ce9d4bf54
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-07-15 17:19:16 +0530

    created classes in accountpage and cartpage

---
+++ vendor_management_webapp/lib/cartpage.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class cartpage extends StatefulWidget {
+  const cartpage({Key? key}) : super(key: key);
+
+  @override
+  State<cartpage> createState() => _cartpageState();
+}
+
+class _cartpageState extends State<cartpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
