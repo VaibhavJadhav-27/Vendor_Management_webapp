8d081e95ad05b6e00d027fb494d52112919a3347
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-07-17 00:12:07 +0530

    chnaged done

--- vendor_management_webapp/lib/homepage.dart
+++ vendor_management_webapp/lib/homepage.dart
@@ -32,6 +32,7 @@ class _HomePageState extends State<HomePage> {
     bool Spices = false;
     bool newspaper = false;
     bool flower = false;
+    bool detect = false;
 
     Future<List<Items>> recommend() async {
       List<Items> recommenditems = [];
@@ -62,7 +63,8 @@ class _HomePageState extends State<HomePage> {
     }
 
     Future<List<Items>> displayitems() async {
-      var url = Uri.parse('http://localhost:4000/items/items');
+      var url = Uri.parse(
+          'http://localhost:4000/items/items/iid/itemname/distinct/fruits');
       if (fruits == true) {
         url = Uri.parse(
             'http://localhost:4000/items/items/iid/itemname/distinct/fruits');
@@ -101,13 +103,15 @@ class _HomePageState extends State<HomePage> {
       };
       var response = await http.get(url, headers: requestHeaders);
       var itemjson = json.decode(response.body);
+      print(itemjson);
       List<Items> item = [];
       for (var u in itemjson) {
         Items menu = Items(u["itemid"], u["itemname"], u["price"],
             u["category"], u["mquantity"], u["vendorid"], u["itemimage"]);
-        itemjson.add(menu);
+        item.add(menu);
       }
       lengthit = item.length;
+      print(item.toString());
       print(lengthit);
       return item;
     }
@@ -235,15 +239,48 @@ class _HomePageState extends State<HomePage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            setState(() {
+                              fruits = !fruits;
+                              detect = true;
+                              vegetables = false;
+                              Spices = false;
+                              dairy = false;
+                              cereals = false;
+                              oils = false;
+                              flower = false;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Fruits",
                             style: TextStyle(
-                              fontSize: 17,
-                            ),
+                                fontSize: 17,
+                                color: (fruits == true)
+                                    ? Colors.black
+                                    : Colors.white),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (vegetables == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              vegetables = !vegetables;
+                              fruits = false;
+                              Spices = false;
+                              dairy = false;
+                              cereals = false;
+                              oils = false;
+                              flower = false;
+                              newspaper = false;
+                              detect = true;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Vegetables",
                             style: TextStyle(
@@ -251,7 +288,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (dairy == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              dairy = !dairy;
+                              vegetables = false;
+                              Spices = false;
+                              fruits = false;
+                              cereals = false;
+                              oils = false;
+                              flower = false;
+                              detect = true;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Dairy Products",
                             style: TextStyle(
@@ -259,7 +314,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (cereals == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              cereals = !cereals;
+                              vegetables = false;
+                              Spices = false;
+                              detect = true;
+                              dairy = false;
+                              fruits = false;
+                              oils = false;
+                              flower = false;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Cereals",
                             style: TextStyle(
@@ -267,7 +340,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (Spices == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              Spices = !Spices;
+                              vegetables = false;
+                              fruits = false;
+                              dairy = false;
+                              detect = true;
+                              cereals = false;
+                              oils = false;
+                              flower = false;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Spices",
                             style: TextStyle(
@@ -275,7 +366,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (oils == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              oils = !oils;
+                              vegetables = false;
+                              Spices = false;
+                              dairy = false;
+                              cereals = false;
+                              fruits = false;
+                              detect = true;
+                              flower = false;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Oils",
                             style: TextStyle(
@@ -283,7 +392,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (flower == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              flower = !flower;
+                              vegetables = false;
+                              Spices = false;
+                              detect = true;
+                              dairy = false;
+                              cereals = false;
+                              oils = false;
+                              fruits = false;
+                              newspaper = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Flowers",
                             style: TextStyle(
@@ -291,7 +418,25 @@ class _HomePageState extends State<HomePage> {
                             ),
                           )),
                       TextButton(
-                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            primary: (newspaper == true)
+                                ? Color.fromRGBO(21, 102, 59, 1)
+                                : Colors.white,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              newspaper = !newspaper;
+                              vegetables = false;
+                              Spices = false;
+                              dairy = false;
+                              cereals = false;
+                              oils = false;
+                              flower = false;
+                              detect = true;
+                              fruits = false;
+                            });
+                            displayitems();
+                          },
                           child: Text(
                             "Newspapers",
                             style: TextStyle(
@@ -321,6 +466,7 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 250,
               child: FutureBuilder(
+                initialData: recommend(),
                 future: recommend(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   return GridView.builder(
@@ -399,11 +545,89 @@ class _HomePageState extends State<HomePage> {
             SizedBox(
               height: 30,
             ),
-            /*FutureBuilder(
-              future: displayitems(),
-              builder:
-                  (BuildContext context, AsyncSnapshot<dynamic> snapshot) {},
-            ),*/
+            SizedBox(
+              height: 250,
+              child: FutureBuilder(
+                future: displayitems(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  return GridView.builder(
+                      //itemCount: lengthit,
+                      scrollDirection: Axis.horizontal,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1),
+                      itemBuilder: (BuildContext context, int index) {
+                        if (detect == false) {
+                          if (snapshot.data == null) {
+                            return Container(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else {
+                            return Text("");
+                          }
+                        } else {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    elevation: 10, primary: Colors.white),
+                                onPressed: () {
+                                  int iid = snapshot.data[index].itemid;
+                                  String iname = snapshot.data[index].itemname;
+                                  String iprice =
+                                      snapshot.data[index].price.toString();
+                                  String icategory =
+                                      snapshot.data[index].category;
+                                  String imqu = snapshot.data[index].mquantity;
+                                  int ivendorid = snapshot.data[index].vendorid;
+                                  String iimage =
+                                      snapshot.data[index].itemimage;
+
+                                  print(iname);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Detailspage(
+                                              profile: user,
+                                              itemname: iname,
+                                              itemid: iid,
+                                              price: iprice,
+                                              category: icategory,
+                                              mquantity: imqu,
+                                              vendorid: ivendorid,
+                                              itemimage: iimage)));
+                                },
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      child: Image.asset(
+                                        snapshot.data[index].itemimage,
+                                        fit: BoxFit.cover,
+                                        //scale: 0.9,
+                                        //height: 50,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10, horizontal: 10),
+                                      child: Text(
+                                        snapshot.data[index].itemname,
+                                        style: TextStyle(
+                                            fontSize: 17, color: Colors.black),
+                                      ),
+                                    )
+                                  ],
+                                )),
+                          );
+                        }
+                      });
+                },
+              ),
+            ),
             SizedBox(
               height: 50,
             )
