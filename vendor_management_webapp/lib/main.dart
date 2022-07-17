f236c5376b33ddb16d8395a301f3c3f64b7e3761
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-07-17 21:49:04 +0530

    done

--- vendor_management_webapp/lib/main.dart
+++ vendor_management_webapp/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: VendorPage(profile: "Vaibhavr"),
+        body: VendorPage(profile: "Vaibhav"),
       ),
       scrollBehavior: MaterialScrollBehavior()
           .copyWith(dragDevices: {PointerDeviceKind.mouse}),
