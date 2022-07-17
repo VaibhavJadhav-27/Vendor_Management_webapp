3f13e47a31f45a309a439ffaf566d27979661484
VaibhavJadhav2703 <43025108+VaibhavJadhav-27@users.noreply.github.com>
2022-07-17 21:56:54 +0530

    vendorpage account button added

--- vendor_management_webapp/lib/vendorpage.dart
+++ vendor_management_webapp/lib/vendorpage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:vendor_management_webapp/accountpage.dart';
 
 class VendorPage extends StatefulWidget {
   final String profile;
@@ -13,6 +14,7 @@ class VendorPage extends StatefulWidget {
 class _VendorPageState extends State<VendorPage> {
   @override
   Widget build(BuildContext context) {
+    String profile = widget.profile;
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
@@ -20,7 +22,54 @@ class _VendorPageState extends State<VendorPage> {
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           color: Colors.blueAccent,
-          child: Center(),
+          child: Center(
+            child: Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  side:
+                      BorderSide(color: Theme.of(context).colorScheme.outline),
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              child: SizedBox(
+                height: 700,
+                width: 700,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Center(
+                        child: Text(
+                      "Welcome " + profile,
+                      style: TextStyle(fontFamily: "WorkSans", fontSize: 30),
+                    )),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) =>
+                                      AccountPage(profile: profile))));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          child: Text(
+                            "Account Info",
+                            style:
+                                TextStyle(fontFamily: "WorkSans", fontSize: 20),
+                          ),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ),
         )),
       ),
     );
